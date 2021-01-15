3d076fd6e96ec9fd16d9e143321b0c8fd6d05a1f
J.Dunkel <joachim95@gmx.at>
2021-01-15 14:14:20 +0100

    now highlights user clicked move..

--- src/js/Ui/userClickMoveHandler.js
+++ src/js/Ui/userClickMoveHandler.js
@@ -6,7 +6,8 @@ class UserClickMoveHandler {
 
     reset(){
         this.fromCellId = null;
-        this.toCellId = null;
+        this.fromCellpreviousBackgroundColor = null;
+
         this.fromPosition = null;
         this.toPosition = null;
         this.userHasAlreadyClickedCell = false;
@@ -17,21 +18,25 @@ class UserClickMoveHandler {
             this.fromCellId = cellId;
             this.fromPosition = position;
             this.userHasAlreadyClickedCell = true;
+            this.highlightCell();
         }
         else{
-            this.toCellId = cellId;
             this.toPosition = position;
             this.viewEvents.trigger([this.fromPosition, this.toPosition]);
+            this.undoCellHighlight();
             this.reset();
         }
     }
 
     highlightCell(){
-        
+        let cell = document.getElementById(this.fromCellId);
+        this.fromCellpreviousBackgroundColor = cell.style.backgroundColor;
+        cell.style.backgroundColor = 'blue';
     }
 
     undoCellHighlight(){
-
+        let cell = document.getElementById(this.fromCellId);
+        cell.style.backgroundColor = this.fromCellpreviousBackgroundColor;
     }
 
 }
