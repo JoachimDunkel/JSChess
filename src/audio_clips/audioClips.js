d74808b97b3b76c08e04ff9a16f982a0a9a13321
J.Dunkel <joachim95@gmx.at>
2021-01-16 16:32:22 +0100

    refactored into class

--- src/audio_clips/audioClips.js
+++ src/audio_clips/audioClips.js
@@ -1,33 +1,31 @@
-function sound(src){
+class Sound {
+    constructor(src){
+        this.src = src;
+        this.sound=document.createElement("audio");
+        this.sound.src=src;
 
- this.sound=document.createElement("audio");
- this.sound.src=src;
+        this.sound.setAttribute("preload","auto");
+        this.sound.setAttribute("controls","none");
+        this.sound.muted="muted";
+        this.sound.style.display="none";
+        document.body.appendChild(this.sound);
+    }
 
- this.sound.setAttribute("preload","auto");
- this.sound.setAttribute("controls","none");
- this.sound.muted="muted";
- this.sound.style.display="none";
- document.body.appendChild(this.sound);
+    play(){
+        this.sound.muted=false;
+        const playPromise=this.sound.play();
+        if(playPromise!==null){
+            playPromise.catch(()=>{this.sound.play();})
+        }
+    }
 
- this.play=function(){
-     this.sound.muted=false;
-     const playPromise=this.sound.play();
-     if(playPromise!==null)
-     {
-       playPromise.catch(()=>{this.sound.play();})
-
-     }
-
- }
-
- this.stop=function(){
-     this.sound.pause();
- }
-
+    stop(){
+        this.sound.pause();
+    }
 }
 
-let music_sound=new sound("audio_clips/lovepiano.mp3");
-let click_sound=new sound("audio_clips/click01.wav");
-let endturn_sound=new sound("audio_clips/endturn.wav");
-let select_sound=new sound("audio_clips/select01.mp3");
-let click02_sound=new sound("audio_clips/click02.wav");
+let music_sound=new Sound("audio_clips/lovepiano.mp3");
+let click_sound=new Sound("audio_clips/click01.wav");
+let endturn_sound=new Sound("audio_clips/endturn.wav");
+let select_sound=new Sound("audio_clips/select01.mp3");
+let click02_sound=new Sound("audio_clips/click02.wav");
