d7b79aeea895612db69f2515220535552153480a
PiotrK <piotr.kupiec@student.tugraz.at>
2021-01-17 10:06:40 +0100

    some styling added

---
+++ src/public/audio_clips/mute.js
@@ -0,0 +1,29 @@
+let checkMusic=document.querySelector('input[value="mute"]');
+checkMusic.addEventListener('change',()=>{
+    if(checkMusic.checked)
+    {
+        music_sound.stop();
+    }
+    else
+    {
+        music_sound.play();
+    }
+});
+document.getElementById("newGameBtn").onclick=
+    function (){
+        if(click_sound.mute!==true) {
+            click_sound.play();
+        }
+        // init();
+    }
+let checkSoundEffects=document.querySelector('input[value="mute_all"]');
+checkSoundEffects.addEventListener('change',()=> {
+    if (checkSoundEffects.checked) {
+        click_sound.mute = true;
+        select_sound.mute = true;
+    }
+    else{
+        click_sound.mute = false;
+        select_sound.mute = false;
+    }
+});
